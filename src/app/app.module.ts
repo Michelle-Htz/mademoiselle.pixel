fd7ccd5836b6fb417eebd52fa4a23ab979f6a6bf
Michelle <michelle-heintze@web.de>
2023-01-28 21:51:45 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { LatestworkComponent } from './latestwork/latestwork.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent,
+    AboutComponent,
+    LatestworkComponent
   ],
   imports: [
     BrowserModule,
